b1581fc6a1730f692bfe12fafb9cffa889744151
Konfederatova_AA <Konfederatova_AA@open.ru>
2020-11-26 17:21:25 +0700

    play_spec

--- spec/states/play_spec.rb
+++ spec/states/play_spec.rb
@@ -21,5 +21,35 @@ describe States::Welcome do
       before { application_context.valeron.health = 0 }
       it { is_expected.to be_a States::Welcome }
     end
+
+    context 'when user inputs "8" (exit)' do
+      before { allow(IOAdapter).to receive(:read).and_return('8') }
+      it { is_expected.to be_a States::Welcome }
+    end
+
+    context 'when user inputs "1"' do
+      before { allow(IOAdapter).to receive(:read).and_return('1') }
+      it { is_expected.to be_a States::Play }
+    end
+
+    context 'when user inputs "2"' do
+      before { allow(IOAdapter).to receive(:read).and_return('2') }
+      it { is_expected.to be_a States::Play }
+    end
+
+    context 'when user inputs "3"' do
+      before { allow(IOAdapter).to receive(:read).and_return('3') }
+      it { is_expected.to be_a States::Play }
+    end
+
+    context 'when user inputs "4"' do
+      before { allow(IOAdapter).to receive(:read).and_return('4') }
+      it { is_expected.to be_a States::Play }
+    end
+
+    context 'when user inputs "9"' do
+      before { allow(IOAdapter).to receive(:read).and_return('9') }
+      it { is_expected.to be_a States::Play }
+    end
   end
 end
